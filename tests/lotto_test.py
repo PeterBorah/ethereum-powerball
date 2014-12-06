07354cc82b306a64a15b33858561794dedd71184
Peter Borah <peterborah@gmail.com>
2014-12-06 13:53:50 -0600

    Implement buying tickets

---
+++ tests/lotto_test.py
@@ -0,0 +1,28 @@
+from eth_tools import Contract, address
+from nose.tools import assert_equal
+from pyethereum import tester as t
+
+INITIALIZE = 0
+SET_PAYOUTS = 1
+BUY_TICKET = 2
+GET_TICKET_OWNER = 3
+GET_TICKET_NUMBERS = 4
+TRANSFER_TICKET = 5
+
+class TestLotto:
+    def setup(self):
+        self.state = t.state()
+        self.contract = Contract("contracts/lotto.se", self.state)
+
+    def test_buying_ticket(self):
+        numbers = [1, 3, 4, 5, 9, 35]
+        ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
+
+        assert_equal(address(self.contract.call(GET_TICKET_OWNER, [ticket_id])[0]), t.a0)
+        assert_equal(self.contract.call(GET_TICKET_NUMBERS, [ticket_id]), numbers)
+
+        new_numbers = [1, 5, 7, 8, 10, 35]
+        new_ticket_id = self.contract.call(BUY_TICKET, new_numbers)[0]
+
+        assert_equal(address(self.contract.call(GET_TICKET_OWNER, [new_ticket_id])[0]), t.a0)
+        assert_equal(self.contract.call(GET_TICKET_NUMBERS, [new_ticket_id]), new_numbers)
