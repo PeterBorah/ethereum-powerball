43ad74ba113a7d46f21229406f5c50d76ace8a9b
Peter Borah <peterborah@gmail.com>
2014-12-06 14:06:02 -0600

    Add test and return value for insufficient payment

--- tests/lotto_test.py
+++ tests/lotto_test.py
@@ -3,7 +3,7 @@ from nose.tools import assert_equal
 from pyethereum import tester as t
 
 INITIALIZE = 0
-SET_PAYOUTS = 1
+SET_CONFIGURATION = 1
 BUY_TICKET = 2
 GET_TICKET_OWNER = 3
 GET_TICKET_NUMBERS = 4
@@ -13,6 +13,7 @@ class TestLotto:
     def setup(self):
         self.state = t.state()
         self.contract = Contract("contracts/lotto.se", self.state)
+        self.contract.call(INITIALIZE)
 
     def test_buying_ticket(self):
         numbers = [1, 3, 4, 5, 9, 35]
@@ -26,6 +27,14 @@ class TestLotto:
 
         assert_equal(address(self.contract.call(GET_TICKET_OWNER, [new_ticket_id])[0]), t.a0)
         assert_equal(self.contract.call(GET_TICKET_NUMBERS, [new_ticket_id]), new_numbers)
+
+    def test_ticket_prices(self):
+        numbers = [1, 3, 4, 5, 9, 35]
+        self.contract.call(SET_CONFIGURATION, [1])
+
+        assert_equal(self.contract.call(BUY_TICKET, numbers, ether=0), [-1])
+        assert_equal(self.contract.call(BUY_TICKET, numbers, ether=1), [0])
+
 
     def test_transfering_ticket(self):
         numbers = [1, 3, 4, 5, 9, 35]
