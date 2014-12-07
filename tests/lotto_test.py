e94cbf59b0a48eef52a68071c5564efa0f20f63c
Peter Borah <peterborah@gmail.com>
2014-12-07 00:56:17 -0600

    Allow admin to explicitly start lotto.

--- tests/lotto_test.py
+++ tests/lotto_test.py
@@ -13,15 +13,18 @@ CLAIM_WINNINGS = 7
 GET_BALANCE = 8
 SET_PAYOUTS = 9
 WITHDRAW = 10
+START_LOTTO = 11
 
 class TestLotto:
     def setup(self):
         self.state = t.state()
         self.contract = Contract("contracts/lotto.se", self.state)
         self.contract.call(INITIALIZE)
+        self.state.mine(1)
 
     def test_buying_ticket(self):
         numbers = [1, 3, 4, 5, 9, 35]
+        self.contract.call(START_LOTTO)
         ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
 
         assert_equal(address(self.contract.call(GET_TICKET_OWNER, [ticket_id])[0]), t.a0)
@@ -35,6 +38,7 @@ class TestLotto:
 
     def test_lotto_closes_after_specified_block(self):
         self.contract.call(SET_CONFIGURATION, [0, 0, 4])
+        self.contract.call(START_LOTTO)
         self.state.mine(5)
         numbers = [1, 3, 4, 5, 9, 35]
 
@@ -43,6 +47,7 @@ class TestLotto:
     def test_ticket_prices(self):
         numbers = [1, 3, 4, 5, 9, 35]
         self.contract.call(SET_CONFIGURATION, [1])
+        self.contract.call(START_LOTTO)
 
         assert_equal(self.contract.call(BUY_TICKET, numbers, ether=0), [-1])
         assert_equal(self.contract.call(BUY_TICKET, numbers, ether=1), [0])
@@ -50,6 +55,7 @@ class TestLotto:
 
     def test_transfering_ticket(self):
         numbers = [1, 3, 4, 5, 9, 35]
+        self.contract.call(START_LOTTO)
         ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
 
         self.contract.call(TRANSFER_TICKET, [ticket_id, t.a1])
@@ -59,6 +65,7 @@ class TestLotto:
     def test_check_winning_numbers(self):
         rng = Contract("contracts/fake_rng.se", self.state)
         self.contract.call(SET_CONFIGURATION, [0, rng.contract, 4])
+        self.contract.call(START_LOTTO)
         assert_equal(self.contract.call(CHECK_WINNERS), [-1])
 
         self.state.mine(5)
@@ -68,6 +75,7 @@ class TestLotto:
         rng = Contract("contracts/fake_rng.se", self.state)
         self.contract.call(SET_CONFIGURATION, [0, rng.contract, 4], ether = 1000)
         self.contract.call(SET_PAYOUTS, [0, 0, 1000, 101, 0, 0, 0, 0, 0, 0])
+        self.contract.call(START_LOTTO)
         numbers = [1, 2, 3, 4, 5, 35]
         ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
 
@@ -80,6 +88,7 @@ class TestLotto:
         rng = Contract("contracts/fake_rng.se", self.state)
         self.contract.call(SET_CONFIGURATION, [0, rng.contract, 4], ether = 1000)
         self.contract.call(SET_PAYOUTS, [0, 0, 1000, 101, 0, 0, 0, 0, 0, 0])
+        self.contract.call(START_LOTTO)
         numbers = [1, 2, 3, 4, 5, 35]
         ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
 
@@ -93,6 +102,7 @@ class TestLotto:
         rng = Contract("contracts/fake_rng.se", self.state)
         self.contract.call(SET_CONFIGURATION, [0, rng.contract, 4], ether = 1000)
         self.contract.call(SET_PAYOUTS, [0, 0, 0, 0, 0, 0, 0, 0, 1000, 0])
+        self.contract.call(START_LOTTO)
         numbers = [1, 2, 5, 6, 7, 1]
         ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
         second_ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
@@ -103,9 +113,28 @@ class TestLotto:
 
     def test_withdrawal_is_only_possible_after_deadline(self):
         self.contract.call(SET_CONFIGURATION, [0, 0, 2, 2], ether = 1000)
+        self.contract.call(START_LOTTO)
 
         assert_equal(self.contract.call(WITHDRAW, [500]), [-1])
 
         self.state.mine(5)
 
         assert_equal(self.contract.call(WITHDRAW, [500]), [500])
+
+    def test_start_new_lotto(self):
+        rng = Contract("contracts/fake_rng.se", self.state)
+        self.contract.call(SET_CONFIGURATION, [0, rng.contract, 2, 2], ether = 1000)
+        self.contract.call(SET_PAYOUTS, [0, 0, 0, 0, 0, 0, 0, 0, 1000, 0])
+        self.contract.call(START_LOTTO)
+        assert_equal(self.contract.call(START_LOTTO), [-1])
+
+        self.state.mine(5)
+
+        assert_equal(self.contract.call(START_LOTTO), [1])
+        numbers = [1, 2, 5, 6, 7, 1]
+        ticket_id = self.contract.call(BUY_TICKET, numbers)[0]
+        assert_equal(ticket_id, 0)
+
+        self.state.mine(5)
+        assert_equal(self.contract.call(CHECK_WINNERS), [1,2,5,6,7,1])
+        assert_equal(self.contract.call(CLAIM_WINNINGS, [ticket_id]), [1000])
